a623e77d02114878c8b735ade69c453124473d5c
harunabernard <harunabernard02@gmail.com>
2023-02-27 06:03:33 -0800

    print in reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,16 +7,16 @@
  */
 void print_rev(char *s)
 {
-	int longi = 0
+	int longi = 0;
 	int o;
 
 	while (*s != '\0')
 	{
-		longi++
-		++;
+		longi++;
+		s++;
 	}
 	s--;
-	for (0 = longi; o > 0; o--)
+	for (o = longi; o > 0; o--)
 	{
 		_putchar(*s);
 		s--;
